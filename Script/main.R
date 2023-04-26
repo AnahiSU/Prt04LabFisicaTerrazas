0e7eb66d3e0be820bedd5ae7d2ad4b4beb34dab1
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2023-04-26 19:19:06 -0400

    Update main.R

--- Script/main.R
+++ Script/main.R
@@ -6,7 +6,7 @@
 #
 #Objetivo: Práctica 4
 
-# #### IPORTANDO LOS DAOTOS #####
+# #### IPORTANDO LOS DATOS #####
 
 par(mfrow = c(3,5))
 
@@ -27,3 +27,15 @@ plot(PresVol$V1, PresVol$P2, type = "o")
 plot(PresVol$V1, PresVol$P3, type = "o")
 plot(PresVol$V1, PresVol$P4, type = "o")
 plot(PresVol$V1, PresVol$P5, type = "o")
+
+
+plot(x = PosTime$t, y = PosTime$x1, ylim = c(1,973), panel.first = grid(col=12))
+points(x= PosTime$t,y=PosTime$x2, pch=3, col=14)
+points(x= PosTime$t,y=PosTime$x3, pch=3, col=15)
+points(x= PosTime$t,y=PosTime$x4, pch=3, col=16)
+points(x= PosTime$t,y=PosTime$x5, pch=3, col=17)
+
+
+points(x=x,y=y2, pch=3, col=14)
+points(x=x,y=y3, pch=5, col=16)
+
