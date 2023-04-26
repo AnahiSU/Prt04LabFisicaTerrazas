fdd248b69d411e619f5725541d2fb7a6a2fa20a3
Anahí Sanabria Ugarte <81058245+AnahiSU@users.noreply.github.com>
2023-04-26 19:55:39 -0400

    linealizar

--- Script/main.R
+++ Script/main.R
@@ -6,27 +6,16 @@
 #
 #Objetivo: Práctica 4
 
-# #### IPORTANDO LOS DATOS #####
+####Funciones####
 
-par(mfrow = c(3,5))
+linealizar = function(t, dist){
+    tiempos = data.frame(t^2)
+    pos = data.frame(dist)
+    res = data.frame("t"=tiempos, "dist" = pos)
+    return(res)
+}
 
-plot(PosTime$t, PosTime$x1, type = "o")
-plot(PosTime$t, PosTime$x2, type = "o")
-plot(PosTime$t, PosTime$x3, type = "o")
-plot(PosTime$t, PosTime$x4, type = "o")
-plot(PosTime$t, PosTime$x5, type = "o")
-
-plot(PosTime2$t, PosTime2$x1, type = "o")
-plot(PosTime2$t, PosTime2$x2, type = "o")
-plot(PosTime2$t, PosTime2$x3, type = "o")
-plot(PosTime2$t, PosTime2$x4, type = "o")
-plot(PosTime2$t, PosTime2$x5, type = "o")
-
-plot(PresVol$V1, PresVol$P1, type = "o")
-plot(PresVol$V1, PresVol$P2, type = "o")
-plot(PresVol$V1, PresVol$P3, type = "o")
-plot(PresVol$V1, PresVol$P4, type = "o")
-plot(PresVol$V1, PresVol$P5, type = "o")
+##### IMPORTANDO LOS DATOS #####
 
 
 plot(x = PosTime$t, y = PosTime$x1, ylim = c(1,973), panel.first = grid(col=12))
@@ -35,7 +24,17 @@ points(x= PosTime$t,y=PosTime$x3, pch=3, col=15)
 points(x= PosTime$t,y=PosTime$x4, pch=3, col=16)
 points(x= PosTime$t,y=PosTime$x5, pch=3, col=17)
 
+plot(x = PosTime$t, y = PosTime$x1, ylim = c(1,973),pch=1, panel.first = grid(col=12))
+points(x= PosTime$t,y=PosTime$x2, pch=2, col=14)
+points(x= PosTime$t,y=PosTime$x3, pch=3, col=15)
+points(x= PosTime$t,y=PosTime$x4, pch=4, col=16)
+points(x= PosTime$t,y=PosTime$x5, pch=5, col=17)
 
-points(x=x,y=y2, pch=3, col=14)
-points(x=x,y=y3, pch=5, col=16)
+legend(0, 800, legend=c("x1", "x2", "x3","x4","x5"),
+       col=c(12,14,15,16,17), pch=c(1,2,3,4,5), cex=1)
+
+tdist = linealizar(PosTime$t, PosTime$x1)
+
+plot(tdist)
+
 
