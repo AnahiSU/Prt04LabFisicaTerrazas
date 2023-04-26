e00de1b1ddf5525027d7b14b027ac1baaf832a40
Anahí <anahi.sanabria@studentambassadors.com>
2023-04-26 18:43:17 -0400

    plots

--- Script/main.R
+++ Script/main.R
@@ -5,3 +5,24 @@
 #Fecha: 27/04/23
 #
 #Objetivo: Práctica 4
+
+par(mfrow = c(3,5))
+
+plot(PosTime$t, PosTime$x1, type = "o")
+plot(PosTime$t, PosTime$x2, type = "o")
+plot(PosTime$t, PosTime$x3, type = "o")
+plot(PosTime$t, PosTime$x4, type = "o")
+plot(PosTime$t, PosTime$x5, type = "o")
+
+
+plot(PosTime2$t, PosTime2$x1, type = "o")
+plot(PosTime2$t, PosTime2$x2, type = "o")
+plot(PosTime2$t, PosTime2$x3, type = "o")
+plot(PosTime2$t, PosTime2$x4, type = "o")
+plot(PosTime2$t, PosTime2$x5, type = "o")
+
+plot(PresVol$V1, PresVol$P1, type = "o")
+plot(PresVol$V1, PresVol$P2, type = "o")
+plot(PresVol$V1, PresVol$P3, type = "o")
+plot(PresVol$V1, PresVol$P4, type = "o")
+plot(PresVol$V1, PresVol$P5, type = "o")
