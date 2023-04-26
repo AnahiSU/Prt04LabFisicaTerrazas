247daa8ad0cdc4cbad4d09a3f78b7ffd380dfa34
Anahí <anahi.sanabria@studentambassadors.com>
2023-04-26 16:52:05 -0400

    Listo para comenzar

---
+++ Report/material/Ejercicio Chp4.R
@@ -0,0 +1,18 @@
+# Nombre: 
+# Cod Syss:
+# Grupo:
+# Carrera:
+# Fecha:
+# 
+# Objetivo: "Graficos y Ecuaciones"
+# En cada uno de los casos:
+# 1) Graficar e indicar que tipo de curva se trata
+# 2) Determinar la ecuacion de ajuste por el metodo grafico
+# 3) graficar una sola imagen por conjunto de matriz de datos, 
+# que incluya el ajuste por metodo grafico,
+# 4) Explicar sus resultados.
+
+# Tres casos
+# caso1
+# Archivos denominados "Postime, y presVol" resolver linealizando, Archivo 
+# "posTime2" resolver por logaritmos.
