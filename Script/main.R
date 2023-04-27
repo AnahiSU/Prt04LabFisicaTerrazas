113b8d07e1aea89fb491a580e35047fe5d03e343
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2023-04-27 18:22:08 -0400

    Update main.R

--- Script/main.R
+++ Script/main.R
@@ -12,21 +12,13 @@ PosTime <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt0
 PosTime2 <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt04270423/Data/PosTime2.csv")
 PresVol <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt04270423/Data/PresVol.csv")
 
-####Funciones####
+#### FUNCIONES ####
 
-linealizar = function(t, dist){
-    tiempos = data.frame(t^2)
-    pos = data.frame(dist)
-    res = data.frame("t"=tiempos, "dist" = pos)
-    return(res)
+# Funcion que recibe los valores de la var dependiente, independiente y el exponente al que se eleva para el cambio de variable
+linealizar = function(indep, dep, exponente){
+    return(data.frame(Z.2 = indep ^ exponente, Y = dep))
 }
-pendiente = function(x2,x1,z2,z1){
-    B = (x2-x1)/(z2-z1)
-    return (B)
-}
-calcA = function(datos){
-    A = min(abs(datos$dist))
-}
+
 linLog = function(t,dist){
     A = log10(dist[which.min(t)])
     B = 2
@@ -43,16 +35,3 @@ ecCurv = function(datos){
     res = c("a" = a, 
             "B" = pend)
 }
-
-##### IMPORTANDO LOS DATOS #####
-
-plot(x = PosTime$t, y = PosTime$x1, ylim = c(1,973),pch=1, panel.first = grid(col=12))
-points(x= PosTime$t,y=PosTime$x2, pch=2, col=14)
-points(x= PosTime$t,y=PosTime$x3, pch=3, col=15)
-points(x= PosTime$t,y=PosTime$x4, pch=4, col=16)
-points(x= PosTime$t,y=PosTime$x5, pch=5, col=17)
-
-legend(0, 800, legend=c("x1", "x2", "x3","x4","x5"),
-       col=c(12,14,15,16,17), pch=c(1,2,3,4,5), cex=1)
-
-
