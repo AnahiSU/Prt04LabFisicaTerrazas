dc6eb60d69e46919f83cf63cf14217a225e11347
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2023-04-27 18:45:40 -0400

    Update main.R

--- Script/main.R
+++ Script/main.R
@@ -1,16 +1,16 @@
 #Author: Leonel Zeballos Aldunate - Anahí Sanabria Ugarte - Mateo Merino Vidal
-#Cod Siss: 
+#Cod Siss: 202300536
 #Carrea: Ingeniería en Informática
 #Grupo: B6
 #Fecha: 27/04/23
 #
 #Objetivo: Práctica 4
 
-####Leer data frames####
+##### IMPORTANDO LOS DATOS #####
 
-PosTime <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt04270423/Data/PosTime.csv")
-PosTime2 <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt04270423/Data/PosTime2.csv")
-PresVol <- read.csv2("C:/Users/MSI i7/Desktop/practicas labo/prt04/202300536Prt04270423/Data/PresVol.csv")
+PosTime <- read.csv2("C:/Users/usser/Escritorio/Leonel/Asignaturas/FISICA_GRAL_LAB/Informes/Informe4_colab/202300047Prt04260423/Data/PosTime.csv")
+PosTime2 <- read.csv2("C:/Users/usser/Escritorio/Leonel/Asignaturas/FISICA_GRAL_LAB/Informes/Informe4_colab/202300047Prt04260423/Data/PosTime2.csv")
+PresVol <- read.csv2("C:/Users/usser/Escritorio/Leonel/Asignaturas/FISICA_GRAL_LAB/Informes/Informe4_colab/202300047Prt04260423/Data/PresVol.csv")
 
 #### FUNCIONES ####
 
@@ -19,19 +19,13 @@ linealizar = function(indep, dep, exponente){
     return(data.frame(Z.2 = indep ^ exponente, Y = dep))
 }
 
-linLog = function(t,dist){
-    A = log10(dist[which.min(t)])
-    B = 2
-    Xprima = log10(t)
-    Yprima = log10(dist)
-    res = data.frame("B" = B,
-                     "A" = A,
-                     "Xprima" = Xprima,
-                     "Yprima" = Yprima)
+linLog = function(indep, dep){
+    return(data.frame(log.X = log10(indep), log.Y = log10(dep)))
 }
+
 ecCurv = function(datos){
-    a = 10^datos[1,2]
-    pend = (datos[99,4]-datos[2,4])/(datos[99,3]-datos[2,3])
-    res = c("a" = a, 
-            "B" = pend)
+    a = 10^datos[2,1]
+    pend = (datos[99,2]-datos[2,2])/(datos[99,1]-datos[2,1])
+    
+    res = c("a" = a, "B" = pend)
 }
